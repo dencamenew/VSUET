e1385bc532f7e37ba8cab88d5debc9ea4f517a65
dmitryvysotskiy <dimavysockij258@gmail.com>
2025-08-13 19:49:24 +0300

    Workversion raiting

--- parsers/raiting/raiting.py
+++ parsers/raiting/raiting.py
@@ -5,6 +5,27 @@ from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import requests
+import logging
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+
+
+
+# Загрузка переменных окружения
+load_dotenv()
+
+
+
+
+# Настройка логирования
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] - [%(levelname)s] --> %(message)s',
+    handlers=[logging.StreamHandler()]
+)
+logger = logging.getLogger(__name__)
 
 
 
@@ -15,8 +36,7 @@ def check_rait(url: str):
     soup = BeautifulSoup(resp.text, 'html.parser')
 
 
-    date = soup.find('span', {'id': 'ucVedBox_lblDateUpdate'}).text.strip()
-    subject = soup.find('span', {'id': 'ucVedBox_lblDis'}).text.strip()
+    sbj = soup.find('span', {'id': 'ucVedBox_lblDis'}).text.strip()
     group_name = soup.find('a', {'id': 'ucVedBox_lblGroup'}).text.strip()
 
 
@@ -24,131 +44,97 @@ def check_rait(url: str):
     ved_type = soup.find_all('span', id="ucVedBox_lblTypeVed")[0].text.strip()
 
 
-    result = {
-        "gruop": group_name,
-        "subject": subject,
-        "last_update": date,
-        "std": []
-    }
-
-
     if ved_type == "Зачет" or ved_type == "Экзамен":
         # тип ведомости по КТ
         if soup.find("input", id="ucVedBox_chkShowKT"): # проверка по наличию input для отображения по КТ
             for row in table_rows:
                 tds = row.find_all('td')
-                std_raiting = {
-                    "num_zach": tds[1].text.strip(),
-                    "raiting": [
-                        tds[7].text.strip() if len(tds) > 7 else "-",
-                        tds[12].text.strip() if len(tds) > 12 else "-",
-                        tds[17].text.strip() if len(tds) > 17 else "-",
-                        tds[22].text.strip() if len(tds) > 22 else "-",
-                        tds[27].text.strip() if len(tds) > 27 else "-",
-                        tds[29].text.strip() if len(tds) > 29 else "-"
-                    ]
-                }
-                result["std"].append(std_raiting)
-
-            return result
+                zach_number = tds[1].text.strip()
+                raiting = [
+                    tds[7].text.strip() if len(tds) > 7 else "-",
+                    tds[12].text.strip() if len(tds) > 12 else "-",
+                    tds[17].text.strip() if len(tds) > 17 else "-",
+                    tds[22].text.strip() if len(tds) > 22 else "-",
+                    tds[27].text.strip() if len(tds) > 27 else "-",
+                    tds[29].text.strip() if len(tds) > 29 else "-"
+                ]
+                cursor.execute("""
+                    INSERT INTO raiting (group_name, zach_number, sbj, raiting)
+                    VALUES (%s, %s, %s, %s)
+                    ON CONFLICT (group_name, zach_number, sbj) 
+                    DO UPDATE SET raiting = EXCLUDED.raiting
+                """, (group_name, zach_number, sbj, raiting))
+            conn.commit()
+            return True
         else:
             # ведомость зачет или экзамен только с оценкой
             for row in table_rows:
                 tds = row.find_all('td')
+                zach_number = tds[2].text.strip()
                 mark = ""
-                if len(tds[4].text.text.strip()) > 0:
+                if len(tds[4].text.strip()) > 0:
                     mark = tds[4].text.strip()
                 else:
                     mark = "-"
-
-                std_raiting = {
-                    "num_zach": tds[2].text.strip(),
-                    "raiting": mark
-                }
-                result["std"].append(std_raiting)
-            return result
+                cursor.execute("""
+                    INSERT INTO raiting (group_name, zach_number, sbj, raiting)
+                    VALUES (%s, %s, %s, %s)
+                    ON CONFLICT (group_name, zach_number, sbj) 
+                    DO UPDATE SET raiting = EXCLUDED.raiting
+                """, (group_name, zach_number, sbj, [mark])) 
+            conn.commit()
+            return True
     else:
         # ведомости с практиками, курсовыми работами и т.д.
         for row in table_rows:
             tds = row.find_all('td')
+            zach_number = tds[2].text.strip()
             mark = ""
-            if len(tds[7].text.text.strip()) > 0:
+            if len(tds[7].text.strip()) > 0:
                 mark = tds[7].text.strip()
-            elif len(tds[4].text.text.strip()) > 0:
+            elif len(tds[4].text.strip()) > 0:
                 mark = tds[4].text.strip()
             else:
                 mark = "-"
-
-            std_raiting = {
-                "num_zach": tds[2].text.strip(),
-                "raiting": mark
-            }
-            result["std"].append(std_raiting)
-
-        return result
+            cursor.execute("""
+                    INSERT INTO raiting (group_name, zach_number, sbj, raiting)
+                    VALUES (%s, %s, %s, %s)
+                    ON CONFLICT (group_name, zach_number, sbj) 
+                    DO UPDATE SET raiting = EXCLUDED.raiting
+                """, (group_name, zach_number, sbj, [mark])) 
+        conn.commit()
+        return True
             
 
 
 
-# настройка драйвера
-chrome_options = Options()
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.page_load_strategy = 'eager'
-driver = webdriver.Chrome(options=chrome_options)
-
-
-url = "https://rating.vsuet.ru/web/Ved/Default.aspx"
-driver.get(url)
-
-
-# Собираем список с названиями всех селектов факультетов
-WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.ID, "ctl00_ContentPage_cmbFacultets"))
-)
-
-faculty_select = Select(WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, "ctl00_ContentPage_cmbFacultets"))
-))
-faculties = [opt.text for opt in faculty_select.options if opt.text and opt.text.strip()]
-
-
-# Переходим по селектам и собираем список всех групп факультета
-for faculty in faculties:
-    faculty_select = Select(WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.ID, "ctl00_ContentPage_cmbFacultets"))
-    ))
-    faculty_select.select_by_visible_text(faculty)
-    
-    WebDriverWait(driver, 10).until(
-        lambda d: Select(d.find_element(By.ID, "ctl00_ContentPage_cmbGroups")).options[0].text != "Выберите группу"
+# Подключение к БД
+try:
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
-    
-    group_select = Select(WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "ctl00_ContentPage_cmbGroups"))
-    ))
-    groups = [opt.text for opt in group_select.options if opt.text and opt.text.strip()]
-    
-    # Переходим по селектам групп --> собирам список с ссылками на предметы --> вызываем функцию со сбором рейтинга
-    for group in groups:
-        group_select = Select(WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "ctl00_ContentPage_cmbGroups"))
-        ))
-        group_select.select_by_visible_text(group)
+    cursor = conn.cursor()
+    logger.info(f"Успешное подключение к БД.")
+except Exception as e:
+    logger.error(f"Ошибка при подключении БД. {e}")
 
-        table = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "ctl00_ContentPage_ucListVedBox_Grid"))
-        )
 
-        links = set()  
-        for link in table.find_elements(By.TAG_NAME, "a"):
-            links.add(link.get_attribute('href'))
+# Получаем urls из БД
+try:
+    cursor.execute("""
+        SELECT urls AS all_urls
+        FROM sbj_urls
+    """)
+    result = cursor.fetchall()
+    urls = [url for tuple_item in result for url in tuple_item[0]]
+    logger.info(f"URL успешно полученны из БД.")
+except Exception as e:
+    logger.error(f"Ошибка при получении данных из БД. {e}")
 
-        for url in links:
-            print(check_rait(url))
-            
 
-        
-driver.quit()
+for url in urls:
+    check_rait(url)
