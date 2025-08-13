2aaa1d29c7ac94e78cd347d3bc9f1e2412191749
dmitryvysotskiy <dimavysockij258@gmail.com>
2025-08-13 14:48:38 +0300

    New table in init

--- db/initdb/init.sql
+++ db/initdb/init.sql
@@ -6,6 +6,14 @@ CREATE TABLE IF NOT EXISTS sbj_urls (
 
 CREATE TABLE IF NOT EXISTS zach (
     id SERIAL PRIMARY KEY,                  
-    zach_number VARCHAR(50) UNIQUE NOT NULL, 
+    zach_number VARCHAR(255) UNIQUE NOT NULL, 
     group_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS raiting (
+    id SERIAL PRIMARY KEY,                  
+    group_name VARCHAR(255) NOT NULL,
+    zach_number VARCHAR(255) NOT NULL, 
+    sbj VARCHAR(255) NOT NULL,
+    raiting TEXT[] NOT NULL
 );
