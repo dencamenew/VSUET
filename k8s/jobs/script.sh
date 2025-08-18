70ba05948f641eb570f33f5ef810db6f2bc9c208
dmitryvysotskiy <dimavysockij258@gmail.com>
2025-08-18 09:04:22 +0300

    скрипт для первого запуска готов

---
+++ k8s/jobs/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# Запускаем Job с расписанием
+kubectl apply -f timetable-job.yaml
+
+# Запускаем Job с ссылками
+kubectl apply -f sdj-job.yaml
+kubectl wait --for=condition=complete --timeout=300s job/sbj-urls || exit 1
+
+# Запускаем Job зачетками
+kubectl apply -f zach-job.yaml
+
+# Запускаем Job с рейтингом
+kubectl apply -f raiting-job.yaml
+
+echo "Все Job'ы запущены!"
