2ff9df0cf15610ec63538c10acbf6f68bdead6dc
dmitryvysotskiy <dimavysockij258@gmail.com>
2025-09-05 12:57:49 +0300

    добавил конструктор

--- teacher/back/teacher-core/src/main/java/ru/practice/teststation/requests/QRGenerationRequest.java
+++ teacher/back/teacher-core/src/main/java/ru/practice/teststation/requests/QRGenerationRequest.java
@@ -1,8 +1,13 @@
 package ru.practice.teststation.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class QRGenerationRequest {
     private String subject;
     private String classTime; // как будет на фронте передаваться хз поэтому пока string 
