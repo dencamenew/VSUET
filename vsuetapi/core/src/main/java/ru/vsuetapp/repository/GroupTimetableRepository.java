4d6e5419ff5586f911edf6d7c79e658d59fa8244
dmitryvysotskiy <dimavysockij258@gmail.com>
2025-10-10 12:41:04 +0300

    рабочая версия

--- vsuetapi/core/src/main/java/ru/vsuetapp/repository/GroupTimetableRepository.java
+++ vsuetapi/core/src/main/java/ru/vsuetapp/repository/GroupTimetableRepository.java
@@ -1,6 +1,8 @@
 package ru.vsuetapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.vsuetapp.model.GroupTimetable;
 
 import java.util.Optional;
@@ -8,6 +10,7 @@ import java.util.Optional;
 public interface GroupTimetableRepository extends JpaRepository<GroupTimetable, Long> {
     GroupTimetable findByGroupId(Long groupId);
 
-    Optional<GroupTimetable> findByGroupName(String groupName);
+    @Query("SELECT gt FROM GroupTimetable gt JOIN gt.group g WHERE g.groupName = :groupName")
+    Optional<GroupTimetable> findByGroupName(@Param("groupName") String groupName);
 }
 
--- vsuetapi/core/src/main/java/ru/vsuetapp/repository/TeacherTimetableRepository.java
+++ vsuetapi/core/src/main/java/ru/vsuetapp/repository/TeacherTimetableRepository.java
@@ -1,14 +1,17 @@
 package ru.vsuetapp.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.vsuetapp.model.TeacherTimetable;
-
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import ru.vsuetapp.model.TeacherTimetable;
 
 @Repository
 public interface TeacherTimetableRepository extends JpaRepository<TeacherTimetable, Long> {
 
-    // поиск расписания по имени преподавателя
-    Optional<TeacherTimetable> findByTeacherName(String teacherName);
+    @Query("SELECT tt FROM TeacherTimetable tt JOIN tt.teacherInfo ti WHERE ti.teacherName = :teacherName")
+    Optional<TeacherTimetable> findByTeacherName(@Param("teacherName") String teacherName);
 }
